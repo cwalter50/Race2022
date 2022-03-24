eb6c81304123a56d316a3049f268bb627c2d3dac
cwalter50 <walt50@gmail.com>
2022-03-24 09:56:13 -0400

    Initial Commit

---
+++ Race2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Race2022
+//
+//  Created by Christopher Walter on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
